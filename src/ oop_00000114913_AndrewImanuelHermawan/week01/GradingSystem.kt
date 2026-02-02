0df965f6a5f85e27829c77acb47c108395a3cb7f
unknown <andrewxhermawan@gmail.com>
2026-02-02 16:08:07 +0700

    week01: initial setup and procedural code

---
+++ src/ oop_00000114913_AndrewImanuelHermawan/week01/GradingSystem.kt
@@ -0,0 +1,11 @@
+package ` oop_00000114913_AndrewImanuelHermawan`.week01
+
+class GradingSystem {
+    fun main() {
+        var name: String = "Andrew Imanuel Hermawan"
+        var score: Int = 80
+
+        println("Nama: " + name + ", nilai: " + score)
+    }
+
+}
