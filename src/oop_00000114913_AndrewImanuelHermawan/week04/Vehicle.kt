6fa434e05bfcdb74c3682146629c1dd42a45c768
unknown <andrewxhermawan@gmail.com>
2026-02-23 15:25:24 +0700

    week04: create basic Vehicle parent class

---
+++ src/oop_00000114913_AndrewImanuelHermawan/week04/Vehicle.kt
@@ -0,0 +1,14 @@
+package oop_00000114913_AndrewImanuelHermawan.week04
+
+open class Vehicle (val brand: String, val model: String ) {
+    var speed : Int = 0
+
+    open fun accelerate() {
+        speed += 10
+        println("$brand melaju. Kecepatan: $speed km/jam")
+    }
+
+    open fun honk() {
+        println("Beep beep!gitgi")
+    }
+}
