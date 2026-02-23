374658a0de8ef8ba4d68ddbb71ff19701f5fb4bc
unknown <andrewxhermawan@gmail.com>
2026-02-23 15:51:45 +0700

    week04: test Vehicle and car in main

--- src/oop_00000114913_AndrewImanuelHermawan/week04/Car.kt
+++ src/oop_00000114913_AndrewImanuelHermawan/week04/Car.kt
@@ -1,6 +1,6 @@
 package oop_00000114913_AndrewImanuelHermawan.week04
 
-open class car(brand: String, val numberOfDoors: Int, model: String) : Vehicle(brand) {
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
 
     fun openTrunk() {
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
---
+++ src/oop_00000114913_AndrewImanuelHermawan/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000114913_AndrewImanuelHermawan.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generateVehicle = Vehicle( brand = "Sepeda Onthel")
+    generateVehicle.honk()
+    generateVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car( brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
