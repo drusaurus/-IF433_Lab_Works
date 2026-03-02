5e219f80cea519ff1077d5597bc6b937cd24ad03
unknown <andrewxhermawan@gmail.com>
2026-03-03 00:22:26 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000114913_AndrewImanuelHermawan/week05/Main.kt
+++ src/oop_00000114913_AndrewImanuelHermawan/week05/Main.kt
@@ -22,4 +22,18 @@ fun main() {
         }
         println("-----------------")
     }
+
+    fun main() {
+
+        val mathHelper = MathHelper()
+
+        val luasPersegi = mathHelper.hitungLuas(4)
+        println("Luas Persegi: $luasPersegi")
+
+        val luasPersegiPanjang = mathHelper.hitungLuas(5, 3)
+        println("Luas Persegi Panjang: $luasPersegiPanjang")
+
+        val luasLingkaran = mathHelper.hitungLuas(7.0)
+        println("Luas Lingkaran: $luasLingkaran")
+    }
 }
