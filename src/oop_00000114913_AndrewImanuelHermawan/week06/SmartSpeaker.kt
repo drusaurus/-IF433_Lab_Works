e68bed933206967e6f477aee3ded205b0e5cc7e4
unknown <andrewxhermawan@gmail.com>
2026-03-11 22:42:23 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000114913_AndrewImanuelHermawan/week06/SmartSpeaker.kt
@@ -0,0 +1,20 @@
+package oop_00000114913_AndrewImanuelHermawan.week06
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("Speaker $name dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("Speaker $name dimatikan.")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+
+}
