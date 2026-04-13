f788aff58ea96fdb062816a484ace322edbafd27
unknown <andrewxhermawan@gmail.com>
2026-04-13 15:56:49 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000114913_AndrewImanuelHermawan/week8/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000114913_AndrewImanuelHermawan.week8;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
