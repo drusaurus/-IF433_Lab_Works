9950d14be86183468a430d5ebbdd7e172cea0476
unknown <andrewxhermawan@gmail.com>
2026-04-13 14:37:01 +0700

    week08: create data collection

--- src/oop_00000114913_AndrewImanuelHermawan/week7/Main.kt
+++ src/oop_00000114913_AndrewImanuelHermawan/week7/Main.kt
@@ -30,8 +30,4 @@ fun main() {
     println("\n=== TEST SEALED CLASS ===")
     val response : ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
-    val uiMessage = when(response) {
-        is ApiResponse.Success -> "Tampilkan: ${response.data}"
-        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
-    }
 }
