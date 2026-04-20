46e4da610c3411c50463d4d38f965ba82c10acac
unknown <andrewxhermawan@gmail.com>
2026-04-20 13:55:10 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000114913_AndrewImanuelHermawan/week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_00000114913_AndrewImanuelHermawan.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -2.5, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 8.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, 0.0, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 30, 22.1, "CLOSED"),
+        TradeLog("SOLUSDT", "SHORT", 5, -10.0, "CLOSED")
+    )
+}
