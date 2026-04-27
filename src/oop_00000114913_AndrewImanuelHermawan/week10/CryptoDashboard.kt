05a62dbc7f432bc4456b6edcc3fb1db152e94fed
unknown <andrewxhermawan@gmail.com>
2026-04-27 14:23:33 +0700

    week10: (task) populate coin repository

--- src/oop_00000114913_AndrewImanuelHermawan/week10/CryptoDashboard.kt
+++ src/oop_00000114913_AndrewImanuelHermawan/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000114913_AndrewImanuelHermawan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
